84181db7a30809213143e0e53b84ae684533e05e
cebeller <cebeller@us.ibm.com>
2021-10-12 14:38:10 -0400

    Add setUp() method to test, class Greeter to implementation

--- app/local/hello_world.py
+++ app/local/hello_world.py
@@ -3,3 +3,13 @@ def hello_world():
 
 def hello_name(name):
     return f'Hello, {name}!'
+
+
+class Greeter:
+
+    def __init__(self, name):
+        self.name = name
+
+    def greet(self):
+        return f'Hello, {self.name}!'
+
--- app/test/hello_world_test.py
+++ app/test/hello_world_test.py
@@ -3,6 +3,10 @@ from app.local import hello_world
 
 class HelloWorldTest(unittest.TestCase):
 
+    def setUp(self):
+        self.greeter = hello_world.Greeter('TDD')
+        
+
     def test_hello_world(self):
         expected = 'Hello, World!'
         message = hello_world.hello_world()
@@ -12,3 +16,15 @@ class HelloWorldTest(unittest.TestCase):
         expected = 'Hello, Nosy!'
         message = hello_world.hello_name('Nosy')
         self.assertEqual(expected, message)
+
+
+    def test_greeter(self):
+        expected = 'Hello, TDD!'
+        message = self.greeter.greet()
+        self.assertEqual(expected, message)
+
+
+
+
+if __name__=='__main__':
+    unittest.main()
